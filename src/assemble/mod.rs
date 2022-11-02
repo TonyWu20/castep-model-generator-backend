e20a20d5dbd912b8016ab5ece68a7798a98eeff3
TonyWu20 <tony.w21@gmail.com>
2022-11-02 12:09:54 +0800

    Change name of the associated type to `T`

--- src/assemble/mod.rs
+++ src/assemble/mod.rs
@@ -6,23 +6,23 @@ use crate::molecule::adsorbate::AdsorbateTraits;
 /// For lattice that can add adsorbate. The adsorbate must implement `AdsorbateTraits` and `Clone`
 pub trait AddAdsorbate {
     /// Generate suffix about adsorbate and coordination sites.
-    fn append_mol_name<P: AdsorbateTraits + Clone>(
+    fn append_mol_name<T: AdsorbateTraits + Clone>(
         &mut self,
-        ads: &P,
+        ads: &T,
         target_sites: &[u32],
         coord_site_dict: &HashMap<u32, String>,
     ) -> Result<(), Box<dyn Error>>;
     /// Initiate the adsorbate orientation before moving to target positions.
-    fn init_ads_direction<P: AdsorbateTraits + Clone>(
+    fn init_ads_direction<T: AdsorbateTraits + Clone>(
         &self,
-        ads: &mut P,
+        ads: &mut T,
         target_sites: &[u32],
         flip_upright: bool,
     ) -> Result<(), Box<dyn Error>>;
     /// Routine to add adsorbate to the lattice.
-    fn add_ads<P: AdsorbateTraits + Clone>(
+    fn add_ads<T: AdsorbateTraits + Clone>(
         &mut self,
-        ads: &mut P,
+        ads: &mut T,
         target_sites: &[u32],
         height: f64,
         flip_upright: bool,
