4c42dd3d78a45c9535a2d53698a96b5e78a3190e
TonyWu20 <tony.w21@gmail.com>
2022-11-02 11:56:08 +0800

    Add `Clone` requirement to `AddAdsorbate`

--- src/assemble/mod.rs
+++ src/assemble/mod.rs
@@ -4,25 +4,25 @@ use std::{collections::HashMap, error::Error};
 use crate::molecule::adsorbate::AdsorbateTraits;
 
 /// For lattice that can add adsorbate
-pub trait AddAdsorbate {
+pub trait AddAdsorbate<P: AdsorbateTraits + Clone> {
     /// Generate suffix about adsorbate and coordination sites.
     fn append_mol_name(
         &mut self,
-        ads: &impl AdsorbateTraits,
+        ads: &P,
         target_sites: &[u32],
         coord_site_dict: &HashMap<u32, String>,
     ) -> Result<(), Box<dyn Error>>;
     /// Initiate the adsorbate orientation before moving to target positions.
     fn init_ads_direction(
         &self,
-        ads: &mut impl AdsorbateTraits,
+        ads: &P,
         target_sites: &[u32],
         flip_upright: bool,
     ) -> Result<(), Box<dyn Error>>;
     /// Routine to add adsorbate to the lattice.
     fn add_ads(
         &mut self,
-        ads: &mut impl AdsorbateTraits,
+        ads: &P,
         target_sites: &[u32],
         height: f64,
         flip_upright: bool,
