60ecf73909f6637f82972b852e46f2cca5ebbca3
TonyWu20 <tony.w21@gmail.com>
2022-11-02 12:07:16 +0800

    Fix associated type position

--- src/assemble/mod.rs
+++ src/assemble/mod.rs
@@ -4,23 +4,23 @@ use std::{collections::HashMap, error::Error};
 use crate::molecule::adsorbate::AdsorbateTraits;
 
 /// For lattice that can add adsorbate. The adsorbate must implement `AdsorbateTraits` and `Clone`
-pub trait AddAdsorbate<P: AdsorbateTraits + Clone> {
+pub trait AddAdsorbate {
     /// Generate suffix about adsorbate and coordination sites.
-    fn append_mol_name(
+    fn append_mol_name<P: AdsorbateTraits + Clone>(
         &mut self,
         ads: &P,
         target_sites: &[u32],
         coord_site_dict: &HashMap<u32, String>,
     ) -> Result<(), Box<dyn Error>>;
     /// Initiate the adsorbate orientation before moving to target positions.
-    fn init_ads_direction(
+    fn init_ads_direction<P: AdsorbateTraits + Clone>(
         &self,
         ads: &mut P,
         target_sites: &[u32],
         flip_upright: bool,
     ) -> Result<(), Box<dyn Error>>;
     /// Routine to add adsorbate to the lattice.
-    fn add_ads(
+    fn add_ads<P: AdsorbateTraits + Clone>(
         &mut self,
         ads: &mut P,
         target_sites: &[u32],
