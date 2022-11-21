fe8efdb43a9b672ee89c415f83c897ae47004fbe
TonyWu20 <tony.w21@gmail.com>
2022-11-21 22:08:46 +0800

    Change adsorbate table fields

--- src/adsorbate/mod.rs
+++ src/adsorbate/mod.rs
@@ -21,8 +21,9 @@ pub struct AdsInfo {
     #[serde(rename = "stemAtomIds")]
     stem_atom_ids: Vec<u32>,
     #[serde(rename = "planeAtomIds")]
-    plane_atom_ids: Vec<u32>,
-    vertical: bool,
+    plane_atom_ids: Option<Vec<u32>>,
+    plane_angle: Option<f64>,
+    stem_angle_at_coord: Option<f64>,
     #[serde(rename = "bSym")]
     symmetric: bool,
     #[serde(rename = "upperAtomId")]
@@ -31,8 +32,6 @@ pub struct AdsInfo {
     atom_nums: u32,
     #[serde(rename = "pathName")]
     path_name: String,
-    rotate_axis_atom_id: Option<u32>,
-    stem_tilted_angle: Option<f64>,
 }
 
 impl AdsInfo {
@@ -46,14 +45,6 @@ impl AdsInfo {
 
     pub fn stem_atom_ids(&self) -> &[u32] {
         self.stem_atom_ids.as_ref()
-    }
-
-    pub fn plane_atom_ids(&self) -> &[u32] {
-        self.plane_atom_ids.as_ref()
-    }
-
-    pub fn vertical(&self) -> bool {
-        self.vertical
     }
 
     pub fn symmetric(&self) -> bool {
@@ -72,11 +63,15 @@ impl AdsInfo {
         self.path_name.as_ref()
     }
 
-    pub fn rotate_axis_atom_id(&self) -> Option<u32> {
-        self.rotate_axis_atom_id
+    pub fn plane_angle(&self) -> Option<f64> {
+        self.plane_angle
     }
 
-    pub fn stem_tilted_angle(&self) -> Option<f64> {
-        self.stem_tilted_angle
+    pub fn stem_angle_at_coord(&self) -> Option<f64> {
+        self.stem_angle_at_coord
+    }
+
+    pub fn plane_atom_ids(&self) -> Option<&Vec<u32>> {
+        self.plane_atom_ids.as_ref()
     }
 }
