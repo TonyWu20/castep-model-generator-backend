c23750d8d8d41418363cde74e648e55bf50f8172
TonyWu20 <tony.w21@gmail.com>
2022-11-22 22:02:09 +0800

    Add error type `ColliearPoints`

--- src/error.rs
+++ src/error.rs
@@ -10,3 +10,15 @@ impl Display for InvalidIndex {
 }
 
 impl Error for InvalidIndex {}
+
+#[derive(Debug)]
+/// Error type when determining plane normal.
+pub struct CollinearPoints;
+
+impl Display for CollinearPoints {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "The given points are on one line!")
+    }
+}
+
+impl Error for CollinearPoints {}
